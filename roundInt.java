a75570d6d2e30ba076895cb68316210dd4876625
Ntokozo <ntokntshangase@gmail.com>
2025-07-02 21:25:03 +0200

    Maths in Java 101

---
+++ roundInt.java
@@ -0,0 +1,9 @@
+public class roundInt
+{
+    public static void main(String[] args)
+    {
+        //printing rounded number in console
+        double num = 5.9;
+        System.out.println("5.9 rounded up: " + Math.round(num));
+    }
+}
